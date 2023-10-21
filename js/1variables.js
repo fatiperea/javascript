a3794c2146eed235456f735ee4212a518d6b2722
fatiperea <fatimaperea3pppp0@gmail.com>
2023-10-21 08:27:31 -0300

    agregamos variables y msj or pantalla y consola

---
+++ js/1variables.js
@@ -0,0 +1,32 @@
+let anioActual= 2023;
+
+const url= "https://universe.rollingcodeschool.com/live/videos/64b5c70978c18f053461e4b5";
+
+console.log("Este es un mensaje en consola");
+
+console.log(anioActual);
+
+console.log(url);
+
+console.log("Año: " + (anioActual+2));
+
+anioActual=2024;
+
+console.log(anioActual);
+
+document.write("Mensaje por pantalla");
+
+document.write( "<h1>Prueba de Titulo</h1>"); 
+
+document.write("Año: " +  anioActual + "<br>");
+
+alert("alerta");
+
+let color= prompt("Ingrese color: ");
+
+console.log(color);
+
+
+
+
+
